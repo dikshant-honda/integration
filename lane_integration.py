ca143a2b2c68fb7e8810cd154a4e1a170a863b11
dikshant-honda <dikshant_s@jp.honda>
2023-06-23 10:40:08 +0900

    added traffic info data

--- lane_integration.py
+++ lane_integration.py
@@ -2,7 +2,9 @@
 
 import rospy
 from perception.code.tests.extract_lane_3d import perception_lane_info
+from perception.code.tests.extract_traffic_3d import traffic_info
 
-info = perception_lane_info()
 
-print(info.image)
+lanes = perception_lane_info()
+traffic = traffic_info()
+print(lanes.image)
